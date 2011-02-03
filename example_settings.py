121b16db5bee053e90f67d44bfcf5487d1ccfc0e
Nikolay Blohin <nikolay@blohin.org>
2011-02-03 15:29:46 +0200

    Add example for settings file

---
+++ example_settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+SECRET_TOKEN = '89376hvjkgh897er589jvio8790b4buj478334563'
+SUBDOMAIN = 'cooldomain'
+ROOM_ID = '375649'
+ROOM_NAME = 'Chating Room'
+
+
